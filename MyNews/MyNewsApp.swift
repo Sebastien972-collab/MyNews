e1d93dc613215a788fd577b263a8843c2371e0e4
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2023-03-08 03:07:27 +0100

    Initial Commit

---
+++ MyNews/MyNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNewsApp.swift
+//  MyNews
+//
+//  Created by Sébastien DAGUIN on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
