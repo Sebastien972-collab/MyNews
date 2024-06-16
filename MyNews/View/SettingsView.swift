c2a24f2b943a2df4a6841363e8da7e1aeb1248bb
Sébastien DAGUIN <sebastien.daguin94@gmail.com>
2024-06-16 15:17:39 +0200

    Ajout de la vue Réglages, modification du titre de la home view en utilisant la toolbar et ajout de la vue bookmark dans settings

---
+++ MyNews/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  MyNews
+//
+//  Created by Sebby on 16/06/2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
